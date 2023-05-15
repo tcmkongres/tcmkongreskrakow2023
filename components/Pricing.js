c490fb44b5a8fdf3c04cb9544d66a40c9ae4920b
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2023-05-15 17:12:55 +0200

    literal w cenie

--- components/Pricing.js
+++ components/Pricing.js
@@ -77,8 +77,10 @@ export default function Pricing() {
           </h2>
         </div>
         <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
-          Mamy dla was nastepujace bilety. Wybierzcie ten, ktory najbardziej wam
-          pasuje.
+          Mamy dla was nastepujące bilety. Bilet normalny, bilet zniżkowy oraz
+          bilet specjalny. Bilet zniżkowy - dla członków ETCMA, bilet specjalny
+          - dla członków PTTMC - Polskiego Towarzystwa Tradycyjnej Medycyny
+          Chińskiej
         </p>
         <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {tiers.map((tier) => (
