f57a3e21930a7ccb4474c3dbfabb6363520f6ab1
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2023-05-16 22:11:32 +0200

    fix koszyka

--- components/Pricing.js
+++ components/Pricing.js
@@ -149,38 +149,6 @@ export default function Pricing() {
             </div>
           ))}
         </div>
-        {tiers.map((tier) => (
-            <div
-                key={tier.id}
-                className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
-            >
-              <div>
-                <h3 id={tier.id} className="text-base font-semibold leading-7 text-indigo-600">
-                  {tier.name}
-                </h3>
-                <div className="mt-4 flex items-baseline gap-x-2">
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">{tier.priceMonthly}</span>
-                  <span className="text-base font-semibold leading-7 text-gray-600">/month</span>
-                </div>
-                <p className="mt-6 text-base leading-7 text-gray-600">{tier.description}</p>
-                <ul role="list" className="mt-10 space-y-4 text-sm leading-6 text-gray-600">
-                  {tier.features.map((feature) => (
-                      <li key={feature} className="flex gap-x-3">
-                        <CheckIcon className="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                        {feature}
-                      </li>
-                  ))}
-                </ul>
-              </div>
-              <a
-                  href={tier.href}
-                  aria-describedby={tier.id}
-                  className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started today
-              </a>
-            </div>
-        ))}
       </div>
     </div>
   );
