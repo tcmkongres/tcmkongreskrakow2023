b7fb14c69e0483987892f69c065e41dd9b1fd4d8
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2023-05-16 22:30:19 +0200

    nowy koszyk

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
-// import Speakers from "@/components/Speakers";
-// import { Schedule } from "@/components/Schedule";
+import Speakers from "@/components/Speakers";
+import { Schedule } from "@/components/Schedule";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
 import Contact from "@/components/Contact";
@@ -9,24 +9,28 @@ import Faqs from "@/components/Faqs";
 import Pricing from "@/components/Pricing";
 import Hotel from "@/components/Hotel";
 import Layout from "app/layout";
+import Koszyk from "@/components/Koszyk";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className="bg-white">
-        <Header></Header>
-        <main>
-          <About></About>
-          {/*<Speakers></Speakers>*/}
-          {/*<Schedule></Schedule>*/}
-          <Hotel></Hotel>
-          <Pricing></Pricing>
-          <Faqs></Faqs>
-          <Organizers></Organizers>
-          <Contact></Contact>
-        </main>
-        <Footer></Footer>
-      </div>
-    </Layout>
+    <>
+      <Layout>
+        <div className="bg-white">
+          <Header></Header>
+          <main>
+            <About></About>
+            {/*<Speakers></Speakers>*/}
+            {/*<Schedule></Schedule>*/}
+            <Hotel></Hotel>
+            {/*<Pricing></Pricing>*/}
+            <Koszyk></Koszyk>
+            <Faqs></Faqs>
+            <Organizers></Organizers>
+            <Contact></Contact>
+          </main>
+          <Footer></Footer>
+        </div>
+      </Layout>
+    </>
   );
 }
